d538754be224e9ea443f502f914c17bd7732d926
bhavsinh-ker <ker.bhavik@gmail.com>
2021-11-01 19:39:01 +0530

    Session 5 -  Featured products and option page

--- wp-content/themes/catalog-site/templates/template-home.php
+++ wp-content/themes/catalog-site/templates/template-home.php
@@ -173,39 +173,87 @@ get_header();
 				?>
 				<!-- Featured Products List -->
 				<?php 
+					$product_image_args = array(
+						"class" => "card-img-top rounded-0"
+					);
+					$product_default_image = get_field( 'product_default_image', 'option' );
+					if ( $product_default_image!="" ) {
+						$product_default_image = wp_get_attachment_image( $product_default_image, 'product-small-thumb', false, $product_image_args );
+					}
+
 					while( $featured_products->have_posts() ) { 
 						$featured_products->the_post();
 						$product_link = get_the_permalink();
+						$product_id = get_the_ID();
+
+						$product_title = get_the_title();
+						if( strlen( $product_title ) > 25 ) {
+							$product_title = substr( $product_title, 0, 22 )."...";
+						}
+
+						$product_image = get_the_post_thumbnail($product_id, 'product-small-thumb', $product_image_args );
+						if ( $product_image=="" ) {
+							$product_image = $product_default_image;
+						}
+						
+						$product_excerpt = get_the_excerpt();
+						if( strlen( $product_excerpt ) > 60 ) {
+							$product_excerpt = substr( $product_excerpt, 0, 60 )."...";
+						}
+
+						$product_price = get_field( 'price', $product_id );
+						if( $product_price!="" ) {
+							$product_price = get_field( 'product_currency', 'option' ).$product_price;
+						}
+
+						$product_colors_terms = get_the_terms( $product_id, 'product-color' );
+						$product_colors = array();
+						if( !is_wp_error( $product_colors_terms ) && !empty( $product_colors_terms ) ) {
+							foreach ( $product_colors_terms as $product_color_term ) {
+								$product_colors[] = $product_color_term->name;
+							}
+							$product_colors = implode( ", ", $product_colors );
+						}
+
+						$product_sizes_terms = get_the_terms( $product_id, 'product-size' );
+						$product_sizes = array();
+						if( !is_wp_error( $product_sizes_terms ) && !empty( $product_sizes_terms ) ) {
+							foreach ( $product_sizes_terms as $product_color_term ) {
+								$product_sizes[] = $product_color_term->name;
+							}
+							$product_sizes = implode( ", ", $product_sizes );
+						}
 					?>
 				<div class="col-md-6 col-lg-3 mb-4 mb-lg-0">
 					<div class="card shadow rounded-0">
 						<a href="<?php echo $product_link; ?>">
-							<?php the_post_thumbnail( 'product-small-thumb', array(
-								"class" => "card-img-top"
-							) ); ?>
+							<?php echo $product_image; ?>
 						</a>
 						<div class="card-body">
 							<a href="<?php echo $product_link; ?>" class="text-dark text-decoration-none">
-								<h5 class="card-title text-capitalize"><?php the_title(); ?></h5>
+								<h5 class="card-title text-capitalize"><?php echo $product_title; ?></h5>
 							</a>
-							<p class="card-text"><?php the_excerpt(); ?></p>
+							<p class="card-text"><?php echo $product_excerpt; ?></p>
 						</div>
 						<ul class="list-group list-group-flush">
 							<li class="list-group-item">
 								<small class="text-muted">
-									<b>Price:</b><span class="float-end"><?php the_field( 'price' ); ?></span>
+									<b><?php _e( 'Price', 'kit_theme' )?>:</b><span class="float-end"><?php echo $product_price; ?></span>
 								</small>
 							</li>
+							<?php if( $product_colors!="" ) { ?>
 							<li class="list-group-item">
 								<small class="text-muted">
-									<b>Color:</b><span class="float-end">Red</span>
+									<b><?php _e( 'Color', 'kit_theme' )?>:</b><span class="float-end"><?php echo $product_colors; ?></span>
 								</small>
 							</li>
+							<?php } if( $product_sizes!="" ) { ?>
 							<li class="list-group-item">
 								<small class="text-muted">
-									<b>Size:</b><span class="float-end">XL</span>
+									<b><?php _e( 'Size', 'kit_theme' )?>:</b><span class="float-end"><?php echo $product_sizes; ?></span>
 								</small>
 							</li>
+							<?php } ?>
 						</ul>
 						<div class="card-body">
 							<a href="<?php echo $product_link; ?>" class="btn btn-outline-primary"><?php _e( 'View Product', 'kit_theme' ); ?></a>
