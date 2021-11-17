34dc1b6297378236ae484f0101b11818e009bfe3
bhavsinh-ker <ker.bhavik@gmail.com>
2021-11-17 12:45:59 +0530

    Session 16 - product filter process

--- wp-content/themes/catalog-site/templates/template-products.php
+++ wp-content/themes/catalog-site/templates/template-products.php
@@ -45,15 +45,16 @@ get_header();
 		"post_type" => "product"
 	);
 
-    $is_product_filter = ( isset( $_GET['product_filter'] ) && $_GET['product_filter'] == "1" ) ? true : false;
-
     /* Filter and sorting process */
+    $is_product_filter = ( isset( $_GET['product_filter'] ) && $_GET['product_filter'] == "1" ) ? true : false;
+    $filter_url_args = array();
     if( $is_product_filter ) {
 
         /* Product Search args */
         $product_search = ( isset( $_GET['product_search'] ) && $_GET['product_search'] != "" ) ? $_GET['product_search'] : false;
         if ( $product_search !== false ) {
             $args["s"] = $product_search;
+            $filter_url_args['product_search'] = $product_search;
         }
         /* EOF Product Search args */
 
@@ -66,24 +67,87 @@ get_header();
                 array(
                     'key'       => 'price',
                     'type'      => 'numeric',
-                ),
+                )
             );
-        }
 
-        if ( $price_min !== false && $price_max === false ) {
-            $args["meta_query"][0]["value"] = $price_min;
-            $args["meta_query"][0]["compare"] = '<=';
-        } elseif( $price_min === false && $price_max !== false ) {
-            $args["meta_query"][0]["value"] = $price_max;
-            $args["meta_query"][0]["compare"] = '>=';
-        } elseif( $price_min !== false && $price_max !== false ) {
-            $args["meta_query"][0]["value"] = array(
-                $price_min,
-                $price_max
-            );
-            $args["meta_query"][0]["compare"] = 'BETWEEN';
+            if ( $price_min !== false && $price_max === false ) {
+                $args["meta_query"][0]["value"] = $price_min;
+                $args["meta_query"][0]["compare"] = '<=';
+                $filter_url_args['price_min'] = $price_min;
+            } elseif( $price_min === false && $price_max !== false ) {
+                $args["meta_query"][0]["value"] = $price_max;
+                $args["meta_query"][0]["compare"] = '>=';
+                $filter_url_args['price_max'] = $price_max;
+            } elseif( $price_min !== false && $price_max !== false ) {
+                $args["meta_query"][0]["value"] = array(
+                    $price_min,
+                    $price_max
+                );
+                $args["meta_query"][0]["compare"] = 'BETWEEN';
+                $filter_url_args['price_min'] = $price_min;
+                $filter_url_args['price_max'] = $price_max;
+            }
         }
         /* EOF Product price filter args */
+
+        /* Product categories filter args */
+        $product_category_filter = ( isset( $_GET['product_category'] ) && ! empty( $_GET['product_category'] ) ) ? $_GET['product_category'] : false;
+        $product_category_tax_query = array();
+        if ( $product_category_filter !== false ) {
+            $product_category_tax_query = array(
+                'taxonomy' => 'product-category',
+                'field'    => 'slug',
+                'terms'    => $product_category_filter
+            );
+            $filter_url_args['product_category'] = $product_category_filter;
+        }
+        /* EOF Product categories filter args */
+
+        /* Product color filter args */
+        $product_color_filter = ( isset( $_GET['product_color'] ) && ! empty( $_GET['product_color'] ) ) ? $_GET['product_color'] : false;
+        $product_color_tax_query = array();
+        if ( $product_color_filter !== false ) {
+            $product_color_tax_query = array(
+                'taxonomy' => 'product-color',
+                'field'    => 'slug',
+                'terms'    => $product_color_filter
+            );
+            $filter_url_args['product_color'] = $product_color_filter;
+        }
+        /* EOF Product color filter args */
+
+        /* Product size filter args */
+        $product_size_filter = ( isset( $_GET['product_size'] ) && ! empty( $_GET['product_size'] ) ) ? $_GET['product_size'] : false;
+        $product_size_tax_query = array();
+        if ( $product_size_filter !== false ) {
+            $product_size_tax_query = array(
+                'taxonomy' => 'product-size',
+                'field'    => 'slug',
+                'terms'    => $product_size_filter
+            );
+            $filter_url_args['product_size'] = $product_size_filter;
+        }
+        /* EOF Product size filter args */
+
+        /* Apply tax query in args */
+        if ( ! empty( $product_category_tax_query ) || ! empty( $product_color_tax_query ) || ! empty( $product_size_tax_query ) ) {
+            if ( ! empty( $product_category_tax_query ) ) {
+                $args['tax_query'][] = $product_category_tax_query;
+            }
+
+            if ( ! empty( $product_color_tax_query ) ) {
+                $args['tax_query'][] = $product_color_tax_query;
+            }
+
+            if ( ! empty( $product_size_tax_query ) ) {
+                $args['tax_query'][] = $product_size_tax_query;
+            }
+            
+            if ( count( $args['tax_query'] ) > 1 ) {
+                $args['tax_query']['relation'] = 'AND';
+            }
+        }
+        /* EOF Apply tax query in args */
 
     }
     /* EOF Filter and sorting process */
@@ -111,7 +175,7 @@ get_header();
                             <div class="card mb-3 rounded-0">
                                 <div class="card-header bg-light"><?php _e( 'Product Search', 'kit_theme' ); ?></div>
                                 <div class="card-body">
-                                    <input type="text" class="form-control" name="product_search" placeholder="<?php _e( 'Enter Product Search', 'kit_theme' ); ?>">
+                                    <input type="text" class="form-control" name="product_search" placeholder="<?php _e( 'Enter Product Search', 'kit_theme' ); ?>" value="<?php echo ( isset($filter_url_args['product_search']) ) ? $filter_url_args['product_search'] : ''; ?>" >
                                 </div>
                               </div>
                             <!-- EOF Product Search -->
@@ -123,8 +187,8 @@ get_header();
                                 </div>
                                 <div class="card-body">
                                     <div class="input-group">
-                                        <input type="number" class="form-control" name="price_min" placeholder="<?php _e( 'Min', 'kit_theme' ); ?>">
-                                        <input type="number" class="form-control" name="price_max" placeholder="<?php _e( 'Max', 'kit_theme' ); ?>">
+                                        <input type="number" class="form-control" name="price_min" placeholder="<?php _e( 'Min', 'kit_theme' ); ?>" value="<?php echo ( isset($filter_url_args['price_min']) ) ? $filter_url_args['price_min'] : ''; ?>">
+                                        <input type="number" class="form-control" name="price_max" placeholder="<?php _e( 'Max', 'kit_theme' ); ?>" value="<?php echo ( isset($filter_url_args['price_max']) ) ? $filter_url_args['price_max'] : ''; ?>">
                                     </div>
                                 </div>
                               </div>
@@ -139,9 +203,13 @@ get_header();
                                 <ul class="list-group list-group-flush">
                                     <?php 
                                         foreach ($product_category as $term) {
+                                            $is_checked = "";
+                                            if ( isset( $filter_url_args['product_category'] ) && in_array( $term->slug, $filter_url_args['product_category'] )) {
+                                                $is_checked = 'checked="checked"';
+                                            }
                                     ?>
                                     <li class="list-group-item">
-                                        <input class="form-check-input me-1" type="checkbox" name="product_category" value="<?php echo $term->slug ?>">
+                                        <input class="form-check-input me-1" type="checkbox" name="product_category[]" value="<?php echo $term->slug ?>" <?php echo $is_checked; ?>>
                                         <?php echo $term->name ?>
                                     </li>
                                     <?php } ?>
@@ -155,9 +223,13 @@ get_header();
                                 <ul class="list-group list-group-flush">
                                     <?php 
                                         foreach ($product_color as $term) {
+                                            $is_checked = "";
+                                            if ( isset( $filter_url_args['product_color'] ) && in_array( $term->slug, $filter_url_args['product_color'] )) {
+                                                $is_checked = 'checked="checked"';
+                                            }
                                     ?>
                                     <li class="list-group-item">
-                                        <input class="form-check-input me-1" type="checkbox" name="product_color" value="<?php echo $term->slug ?>">
+                                        <input class="form-check-input me-1" type="checkbox" name="product_color[]" value="<?php echo $term->slug ?>" <?php echo $is_checked; ?>>
                                         <?php echo $term->name ?>
                                     </li>
                                     <?php } ?>
@@ -171,9 +243,13 @@ get_header();
                                 <ul class="list-group list-group-flush">
                                     <?php 
                                         foreach ($product_size as $term) {
+                                            $is_checked = "";
+                                            if ( isset( $filter_url_args['product_size'] ) && in_array( $term->slug, $filter_url_args['product_size'] )) {
+                                                $is_checked = 'checked="checked"';
+                                            }
                                     ?>
                                     <li class="list-group-item">
-                                        <input class="form-check-input me-1" type="checkbox" name="product_size" value="<?php echo $term->slug ?>">
+                                        <input class="form-check-input me-1" type="checkbox" name="product_size[]" value="<?php echo $term->slug ?>" <?php echo $is_checked; ?>>
                                         <?php echo $term->name ?>
                                     </li>
                                     <?php } ?>
